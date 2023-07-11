d8864299785ea2af534179adbc7213d37b4105aa
salino3 <flaviobcn012@gmail.com>
2023-07-11 11:56:23 +0200

    add Card component

---
+++ components/Card.vue
@@ -0,0 +1,84 @@
+<template>
+     <li v-for="item in list" :key="item.id">
+        <NuxtLink :to="`/product/${item.id}`">
+       <article class="grid product-container card"
+          :class="{
+            'product-container--has-discount': item.discountPercentage > 15,
+          }"
+        >
+          <div class="image">
+            <img :src="item?.images[0]" alt="product image" loading="lazy" />
+          </div>
+          <div class="product-container__content">
+            <h2>
+              {{ item?.title }}
+            </h2>
+            <p>
+              <span class="grey-text">Description: </span>
+              <strong>{{ item?.description }}</strong>
+            </p>
+            <p>
+              <span class="grey-text">Brand: </span>
+              {{ item.brand }}
+            </p>
+            <p><span class="grey-text">Category: </span>{{ item.category }}</p>
+          </div>
+          <div class="flex product-container__aside">
+            <div class="text-align-end aside__price">
+              <StaticPrice
+                :quantity="item.price"
+                :coin="item.brand === 'Apple' ? 'USD' : 'EUR'"
+              />
+            </div>
+            <AddToCartButton :product="item" @addItem="onAddItem($event)"/>
+          </div>
+        </article>
+       </NuxtLink>
+      </li>
+</template>
+
+<script setup lang="ts">
+import { productService } from "@/services/all-products";
+import { Product } from "../all-types/index";
+
+const list: Product[] = await productService.getAll();
+
+ const onAddItem = (product: Product) => {
+   console.log('onAddItem', product)
+ }
+</script>
+
+<style lang="scss" scope>
+ 
+.product-container {
+  align-items: flex-start;
+  grid-template-columns: 210px 1fr 100px;
+}
+
+.product-container__content {
+  padding: 0 1em;
+}
+
+.product-container__aside {
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.image {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+
+  img {
+    width: 100%;
+    aspect-ratio: 1/1;
+    object-fit: cover;
+  }
+}
+
+.product-container--has-discount {
+  background-color: rgba(yellow, 0.5);
+}
+</style>
--- components/ProductList.vue
+++ components/ProductList.vue
@@ -5,43 +5,7 @@
       total: {{ totalProducts }}
     </div>
     <ul class="product-list">
-      <li v-for="item in list" :key="item.id">
-        <NuxtLink :to="`/product/${item.id}`">
-       <article class="grid product-container card"
-          :class="{
-            'product-container--has-discount': item.discountPercentage > 15,
-          }"
-        >
-          <div class="image">
-            <img :src="item?.images[0]" alt="product image" loading="lazy" />
-          </div>
-          <div class="product-container__content">
-            <h2>
-              {{ item?.title }}
-            </h2>
-            <p>
-              <span class="grey-text">Description: </span>
-              <strong>{{ item?.description }}</strong>
-            </p>
-            <p>
-              <span class="grey-text">Brand: </span>
-              {{ item.brand }}
-            </p>
-            <p><span class="grey-text">Category: </span>{{ item.category }}</p>
-          </div>
-          <div class="flex product-container__aside">
-            <div class="text-align-end aside__price">
-              <StaticPrice
-                :quantity="item.price"
-                :coin="item.brand === 'Apple' ? 'USD' : 'EUR'"
-              />
-            </div>
-            <AddToCartButton :product="item" @addItem="onAddItem($event)"/>
-          </div>
-        </article>
-       </NuxtLink>
-
-      </li>
+        <Card  />  
     </ul>
   </section>
 </template>
@@ -56,9 +20,6 @@ const totalProducts: globalThis.ComputedRef<number> = computed(
   () => list.length
 );
 
- const onAddItem = (product: Product) => {
-   console.log('onAddItem', product)
- }
 </script>
 
 <style lang="scss" scoped>
@@ -70,35 +31,4 @@ const totalProducts: globalThis.ComputedRef<number> = computed(
   }
 }
 
-.product-container {
-  align-items: flex-start;
-  grid-template-columns: 210px 1fr 100px;
-}
-
-.product-container__content {
-  padding: 0 1em;
-}
-
-.product-container__aside {
-  flex-direction: column;
-  justify-content: space-between;
-  height: 100%;
-}
-
-.image {
-  display: flex;
-  height: 100%;
-  flex-direction: column;
-  justify-content: center;
-
-  img {
-    width: 100%;
-    aspect-ratio: 1/1;
-    object-fit: cover;
-  }
-}
-
-.product-container--has-discount {
-  background-color: rgba(yellow, 0.5);
-}
 </style>
